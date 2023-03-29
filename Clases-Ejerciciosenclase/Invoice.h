b19f5b8a7b48d814bab0b030908a5f55374c2ece
Alumno B303 <Alumno-B303@UCSP.AULAB>
2023-03-29 08:30:25 -0500

    Ejercicios en clase

---
+++ Clases-Ejerciciosenclase/Invoice.h
@@ -0,0 +1,9 @@
+#include <string>
+
+class Invoice{
+public:
+    explicit Invoice(std::string invoiceNumber, std::string invoiceDescription, int invoiceQuantity, int invoicePrice, double invoiceVAT, double invoiceDiscount) : 
+        name{accountName} {
+            if(initialBalance > 0){
+private:
+};
